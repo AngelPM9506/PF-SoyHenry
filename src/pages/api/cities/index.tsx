b6c62b9c95d4796daa51c6909d5509454db7b288
SantinoFajardo <santinofajardo02@gmail.com>
2022-09-13 15:53:59 -0300

    cities data base and routes

--- src/pages/api/cities/index.tsx
+++ src/pages/api/cities/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from "src/utils/prisma";
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+
+export default async function Cities(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  switch (method) {
+    case 'GET':
+      try {
+        let response = await prisma.city.findMany()
+        return res.json(response)
+      } catch (error) {
+        return res.json(error)
+      }
+    default:
+      return res.status(400).json({ msg: 'Method not supported, try again' })
+  }
 }
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,23 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 const cities = require('all-the-cities');
-import {City, CityInDB} from 'src/utils/interface'
+import { City, CityInDB } from 'src/utils/interface'
 import prisma from 'src/utils/prisma';
 
 
-type Data = {
-	msg?: string
-}
+// type Data = {
+// 	msg?: string
+// }
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<Data>
+	res: NextApiResponse
 ) {
-	const {method} = req;
+	const { method } = req;
 
 	switch (method) {
 		case 'GET':
 			const response = cities.filter((city: City) => city.population > 100000)
-			let citiesCreatedInDB = response.map((c: City)=> (
+			let citiesCreatedInDB = response.map((c: City) => (
 				{
 					name: c.name,
 					country: c.country,
@@ -31,7 +31,7 @@ export default async function handler(
 			try {
 				const response = await prisma.$transaction(citiesCreatedInDB.map((c: CityInDB) => (
 					prisma.city.upsert({
-						where: { name: c.name},
+						where: { name: c.name },
 						update: {},
 						create: {
 							name: c.name,
@@ -47,6 +47,6 @@ export default async function handler(
 				return res.status(404).json(error)
 			}
 		default:
-			return res.status(400).send({msg: 'Method not supported try again'})
+			return res.status(400).send({ msg: 'Method not supported try again' })
 	}
 }

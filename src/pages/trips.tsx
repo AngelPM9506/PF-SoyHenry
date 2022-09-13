47d921131194e4ad151e229bdc3e635aa885b1da
dinucciagus <dinucciagus@gmail.com>
2022-09-13 14:17:51 -0300

    key added to he cards inside trips page

--- src/pages/trips.tsx
+++ src/pages/trips.tsx
@@ -3,19 +3,21 @@ import { TripCard } from "../components/TripCard";
 import { Trip } from "src/interfaces/Trip";
 
 interface Props {
-  Trips: Trip[];
+  trips: Trip[];
 }
 
-export default function Trips(props: Props) {
-  return props.Trips.length === 0 ? (
-    <div>
-      <h1>There are no trips yet! </h1>
-    </div>
-  ) : (
+export default function Trips({ trips }: Props) {
+  return (
+    //  props.Trips.length === 0 ? (
+    //   <div>
+    //     <h1>There are no trips yet! </h1>
+    //   </div>
+    // ) :
+    // (
     <div>
       <h1>All the trips will be here!</h1>
-      {props.Trips.map((t) => (
-        <TripCard props={t} />
+      {trips.map((t) => (
+        <TripCard key={t.id} props={t} />
       ))}
     </div>
   );
@@ -24,6 +26,7 @@ export default function Trips(props: Props) {
 export const getServerSideProps = async () => {
   const res = await fetch("http://localhost:3000/api/trips"); //chequear que endPoint eligen los chicos en Api
   const trips = await res.json();
+  console.log(trips);
   return {
     props: {
       trips: trips,

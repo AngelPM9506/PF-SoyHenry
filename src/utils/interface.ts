123f98d8fdf39e17b1ed8a13d47b86fbd30ecd1e
MauroPerna <maurofabricioperna@gmail.com>
2022-09-13 15:19:22 -0300

    add admin and cities by default when lifting the DB

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -8,3 +8,29 @@ export interface UserUpdate {
     name: string;
     description: string;
 }
+
+
+export interface City {
+    cityId: String,
+    name: String,
+    country: String,
+    altCountry: String,
+    muni: String,
+    muniSub: String,
+    featureClass: String,
+    featureCode: String,
+    adminCode: String,
+    population: Number,
+    loc: {
+        type: String,
+        coordinates: Number[] 
+    }
+}
+
+export interface CityInDB {
+    name: string,
+	country: string,
+	population: number,
+	latitude: number,
+	longitude: number
+}

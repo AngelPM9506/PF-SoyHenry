148e4aed0aaf8577068031ae69ad932fe50b1613
dinucciagus <dinucciagus@gmail.com>
2022-09-13 12:40:13 -0300

    Component Card Trip created

---
+++ src/components/TripCard.tsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+  Image,
+  Button,
+} from "@chakra-ui/react";
+
+export function TripCard(props: any) {
+  return (
+    <Center py={12}>
+      <Box
+        role={"group"}
+        p={6}
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
+        zIndex={1}
+      >
+        <Box
+          rounded={"lg"}
+          mt={-12}
+          pos={"relative"}
+          height={"230px"}
+          _after={{
+            transition: "all .3s ease",
+            content: '""',
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}
+        >
+          <Image
+            rounded={"lg"}
+            height={230}
+            width={282}
+            objectFit={"cover"}
+            src={props.image}
+          />
+        </Box>
+        <Stack pt={10} align={"center"}>
+          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+            {props.name}
+          </Heading>
+          <Stack direction={"row"} align={"center"}>
+            <Text fontWeight={800} fontSize={"xl"}>
+              Duration: from {props.initDate} to {props.endDate}
+            </Text>
+            <Text fontWeight={800} fontSize={"xl"}>
+              N of Travelers: {props.tripOnUser.length}
+            </Text>
+            <Text fontWeight={800} fontSize={"xl"}>
+              Activities: {props.activities.map((a: any) => a.name)}
+            </Text>
+            <Text fontWeight={800} fontSize={"xl"}>
+              {props.price}
+            </Text>
+          </Stack>
+        </Stack>
+        <Button
+          w={"full"}
+          mt={8}
+          bg={useColorModeValue("#151f21", "gray.900")}
+          color={"white"}
+          rounded={"md"}
+          _hover={{
+            transform: "translateY(-2px)",
+            boxShadow: "lg",
+          }}
+        >
+          Join the Trip!
+        </Button>
+      </Box>
+    </Center>
+  );
+}

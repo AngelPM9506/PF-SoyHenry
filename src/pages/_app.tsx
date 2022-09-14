c3c58a18ad1303a89b6bee7378257fad85bd54c2
MauroPerna <maurofabricioperna@gmail.com>
2022-09-14 12:06:04 -0300

    finish cloudinary config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,29 +6,35 @@ import { myTheme } from "src/styles/theme";
 import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-function MyApp({ Component, pageProps }: AppProps) {
-const queryClient = new QueryClient();
- const [showChild, setShowChild] = useState(false);
-  useEffect(() => {
-    setShowChild(true);
-  }, []);
 
-  if (!showChild) {
-    return null;
-  }
-   if (typeof window === "undefined") {
-    return <></>;
-  } else {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <UserProvider>
-          <Component {...pageProps} />
-          <ReactQueryDevtools />
-        </UserProvider>
-      </ChakraProvider>
-    </QueryClientProvider>
-  );
-  }
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+	const queryClient = new QueryClient();
+	const [showChild, setShowChild] = useState(false);
+	useEffect(() => {
+		setShowChild(true);
+	}, []);
+
+	if (!showChild) {
+		return null;
+	}
+	if (typeof window === "undefined") {
+		return <></>;
+	} else {
+		return (
+			<QueryClientProvider client={queryClient}>
+				<ChakraProvider>
+					<UserProvider>
+						<Component {...pageProps} />
+						<ReactQueryDevtools />
+					</UserProvider>
+				</ChakraProvider>
+			</QueryClientProvider>
+		);
+	}
+}
+
 
 export default MyApp;

f0f43d6d0dc9343616d20c88b651460be79ccbdf
SantinoFajardo <santinofajardo02@gmail.com>
2022-09-13 21:37:59 -0300

    relacion en city-activities

--- src/pages/api/activities/index.tsx
+++ src/pages/api/activities/index.tsx
@@ -3,7 +3,7 @@ import { weekdays } from "src/utils/interface";
 import prisma from "src/utils/prisma";
 
 export default async function index(req: NextApiRequest, res: NextApiResponse) {
-    let { method, body: { name, availability, description, price } } = req;
+    let { method, body: { name, availability, description, price, cityName } } = req;
     switch (method) {
         /**obtener todos las actividades */
         case 'GET':
@@ -16,8 +16,17 @@ export default async function index(req: NextApiRequest, res: NextApiResponse) {
         /**agregar una nueva actividad */
         case 'POST':
             /**Comprobar que existan los argumentos y que availability este en el enum*/
-            if (!name || !availability || !Object.values(weekdays).includes(availability) || !description || !price) return res.status(400).json({ msg: 'Missing data, try again' });
-            let activity = { data: { name, availability, description, price } };
+            if (!name ||
+                !availability ||
+                !Object.values(weekdays).includes(availability) ||
+                !description ||
+                !price ||
+                !cityName) {
+                return res.status(400).json({ msg: 'Missing data, try again' })
+            };
+            let searchCity = await prisma.city.findMany({ where: { name: cityName } })
+            let cityId = searchCity[0].id
+            let activity = { data: { name, availability, description, price, cityId } };
             try {
                 let response = await prisma.activity.create(activity);
                 return res.status(201).json(response);

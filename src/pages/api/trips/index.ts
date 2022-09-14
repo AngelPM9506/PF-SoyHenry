a07d534924f037ec43cbf0976f45e3f93a549729
SantinoFajardo <santinofajardo02@gmail.com>
2022-09-14 12:30:40 -0300

    Relation trips/traveleres

--- src/pages/api/trips/index.ts
+++ src/pages/api/trips/index.ts
@@ -16,18 +16,9 @@ export default async function index(
                 return res.status(500).json({ error: error.message });
             }
         case 'POST':
-            if (!name || !initDate || !endDate || !description || !price || !planner) return res.status(400).json({ msg: 'Missing data, try again' })
-            // planner debe ser el id del usuario que esta creando el viaje
-            /*
-            {
-                "name": "Rio Cuarto para la people",
-                "initDate": "2022-10-01",
-                "endDate": "2022-10-15",
-                "description": "description",
-                "price": 5.5,
-                "planner": "cl7zdjn89001178v0hq363xub"
+            if (!name || !initDate || !endDate || !description || !price || !planner) {
+                return res.status(400).json({ msg: 'Missing data, try again' })
             }
-            */
             let initialDate = new Date(initDate)
             let finishDate = new Date(endDate)
             try {

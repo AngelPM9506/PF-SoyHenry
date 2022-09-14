be094966ade4c2d2f41d290485b908c09e4c19e6
JonathanJB88 <Jonajes0288@gmail.com>
2022-09-15 01:16:07 +0200

    Missing bracket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,8 @@ import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 function MyApp({ Component, pageProps }: AppProps) {
-const queryClient = new QueryClient();
- const [showChild, setShowChild] = useState(false);
+  const queryClient = new QueryClient();
+  const [showChild, setShowChild] = useState(false);
   useEffect(() => {
     setShowChild(true);
   }, []);
@@ -16,19 +16,20 @@ const queryClient = new QueryClient();
   if (!showChild) {
     return null;
   }
-   if (typeof window === "undefined") {
+  if (typeof window === "undefined") {
     return <></>;
   } else {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <UserProvider>
-          <Component {...pageProps} />
-          <ReactQueryDevtools />
-        </UserProvider>
-      </ChakraProvider>
-    </QueryClientProvider>
-  );
+    return (
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <UserProvider>
+            <Component {...pageProps} />
+            <ReactQueryDevtools />
+          </UserProvider>
+        </ChakraProvider>
+      </QueryClientProvider>
+    );
   }
+}
 
 export default MyApp;

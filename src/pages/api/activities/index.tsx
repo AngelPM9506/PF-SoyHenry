4a8d4b56fe00aaa502df6ec0d723d26e4b41c60f
AngelPM9506 <parra_mondragon@live.com>
2022-09-16 11:12:43 -0500

    Refactorizando...

--- src/pages/api/activities/index.tsx
+++ src/pages/api/activities/index.tsx
@@ -8,70 +8,67 @@ export default async function index(req: NextApiRequest, res: NextApiResponse) {
     body: { name, availability, description, price, cityName, image },
     query: { wName, sort, sortBy, wCity, maxPrice },
   } = req;
-  switch (method) {
-    /**obtener todos las actividades */
-    case 'GET':
-      let orderBy: typeSort[] = [];
-      let sortfrom: typeSort = {};
-      /**http://127.0.0.1:3000/api/activities?sort=asc&sortBy=price&wName=nadar&wCity=Dubai&maxPrice=60*/
-      let sortName: string = sortBy ? sortBy.toString().toLowerCase() : 'name';
-      sortfrom[sortName] = sort ? sort.toString().toLowerCase() : 'desc';
-      orderBy.push(sortfrom);
-      let condition: condition = {
-        where: wName ? { name: { contains: wName.toString() } } : {},
-        include: { city: true },
-        orderBy,
-      };
-      wCity
-        ? (condition.where = {
-            ...condition.where,
-            city: { is: { name: { contains: wCity.toString() } } },
-          })
-        : '';
-      maxPrice
-        ? (condition.where = {
-            ...condition.where,
-            price: { lte: parseFloat(maxPrice.toString()) },
-          })
-        : '';
+  try {
+    switch (method) {
+      /**obtener todos las actividades */
+      case 'GET':
+        let orderBy: typeSort[] = [];
+        let sortfrom: typeSort = {};
+        /**http://127.0.0.1:3000/api/activities?sort=asc&sortBy=price&wName=nadar&wCity=Dubai&maxPrice=60*/
+        let sortName: string = sortBy ? sortBy.toString().toLowerCase() : 'name';
+        sortfrom[sortName] = sort ? sort.toString().toLowerCase() : 'desc';
+        orderBy.push(sortfrom);
 
-      try {
-        let response = await prisma.activity.findMany(condition);
-        return res.status(200).json(response);
-      } catch (error: any) {
-        return res.status(400).json({ msg: error.message });
-      }
-    /**agregar una nueva actividad */
-    case 'POST':
-      /**Comprobar que existan los argumentos y que availability este en el enum*/
-      if (
-        !name ||
-        !availability ||
-        !Object.values(weekdays).includes(availability) ||
-        !description ||
-        !price ||
-        !cityName ||
-        !image
-      ) {
-        return res.status(400).json({ msg: 'Missing data, try again' });
-      }
+        let condition: condition = {
+          where: wName ? { name: { contains: wName.toString() } } : {},
+          include: { city: true },
+          orderBy,
+        };
 
-      let searchCity = await prisma.city.findMany({
-        where: { name: cityName },
-      });
-      let cityId = searchCity[0].id;
-      let activity = {
-        data: { name, availability, description, price, cityId, image },
-      };
-      try {
-        let response = await prisma.activity.create(activity);
+        wCity ? (condition.where = {
+          ...condition.where,
+          city: { is: { name: { contains: wCity.toString() } } },
+        }) : '';
 
-        return res.status(201).json(response);
-      } catch (error: any) {
-        return res.status(400).json({ msg: error.message });
-      }
-    default:
-      res.status(400).send('Metohd not supported try again');
-      break;
+        maxPrice ? (condition.where = {
+          ...condition.where,
+          price: { lte: parseFloat(maxPrice.toString()) },
+        }) : '';
+
+        var responseG = await prisma.activity.findMany(condition);
+        return res.status(200).json(responseG);
+
+      /**agregar una nueva actividad */
+      case 'POST':
+        /**Comprobar que existan los argumentos y que availability este en el enum*/
+        if (
+          !name || !availability || !description || !price || !cityName || !image
+        ) { return res.status(400).json({ msg: 'Missing data, try again' }); }
+
+        let activity = {
+          data: {
+            name, availability, description, price, image,
+            city: { connect: { name: cityName.toString() } }
+          },
+          include: { city: true }
+        };
+        var responseP = await prisma.activity.create(activity);
+        return res.status(201).json(responseP);
+      default:
+        res.status(400).send('Metohd not supported try again');
+        break;
+    }
+  } catch (error: any) {
+    return res.json({ status: "error", msg: error.message });
   }
 }
+/*
+{
+  "name": "Nadar en un arecife de coral",
+  "availability": ["Monday", "Tuesday"],
+  "description": "Un gran día en el mar",
+  "price": 30.65,
+  "cityName": "Texcoco de Mora",
+  "image": "Prueba imagen",
+}
+*/

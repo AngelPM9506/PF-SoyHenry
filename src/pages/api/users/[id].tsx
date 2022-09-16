ecf1ebc8c024acad7f11552da15b9a25cedde1f2
Alejandro <alejandromanuelhernandez@gmail.com>
2022-09-16 11:34:50 -0300

    Fix userprofile put

--- src/pages/api/users/[id].tsx
+++ src/pages/api/users/[id].tsx
@@ -1,111 +1,103 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from 'src/utils/prisma';
-import { UserData } from 'src/components/UserProfile';
-import cloudinary from 'src/utils/cloudinary';
-const {CLOUDINARY_PRESET_AVATARS} = process.env;
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "src/utils/prisma";
+import { UserData } from "src/components/UserProfile";
+import cloudinary from "src/utils/cloudinary";
+import { toNamespacedPath } from "path";
+import { cloudinaryImg } from "src/utils/cloudinaryUpload";
+const { CLOUDINARY_PRESET_AVATARS } = process.env;
 
 export default async function index(req: NextApiRequest, res: NextApiResponse) {
-	const {
-		method,
-		body: {
-			name,
-			description,
-			mail,
-			avatar,
-			keyWords,
-			urlTikTok,
-			urlInstagram,
-			urlFaceBook,
-		},
-		query: { id },
-	} = req;
+  const {
+    method,
+    body: {
+      name,
+      description,
+      mail,
+      avatar,
+      keyWords,
+      urlTikTok,
+      urlInstagram,
+      urlFaceBook,
+    },
+    query: { id },
+  } = req;
 
-	switch (method) {
-		case 'GET':
-			try {
-				const response = await prisma.user.findUnique({
-					where: { id: id?.toString() },
-				});
-				if (response.active) {
-					return res.status(200).json(response);
-				} else {
-					return res
-						.status(404)
-						.json({ msg: 'Inactive user contact the admin' });
-				}
-			} catch (error: any) {
-				return res.status(500).json({ error: error.message });
-			}
-		case 'PUT':
-			if (!name || !description || !mail)
-				return res.status(400).json({ msg: 'Missing data, try again' });
-			let user: UserData = {
-				name,
-				description,
-				mail,
-				avatar,
-				urlTikTok,
-				urlInstagram,
-				urlFaceBook,
-				keyWords,
-			};
+  switch (method) {
+    case "GET":
+      try {
+        const response = await prisma.user.findUnique({
+          where: { id: id?.toString() },
+        });
+        if (response.active) {
+          return res.status(200).json(response);
+        } else {
+          return res
+            .status(404)
+            .json({ msg: "Inactive user contact the admin" });
+        }
+      } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+      }
+    case "PUT":
+      if (!name || !description || !mail)
+        return res.status(400).json({ msg: "Missing data, try again" });
+      let user: UserData = {
+        name,
+        description,
+        mail,
+        avatar,
+        urlTikTok,
+        urlInstagram,
+        urlFaceBook,
+        keyWords,
+      };
 
-			try {
-
-				const uploadImage = await cloudinary.uploader.upload(user.avatar,
-					{
-						upload_preset: CLOUDINARY_PRESET_AVATARS, 
-						public_id: `${user.mail}-avatar:${Date.now()}`,
-						allowed_formats: ['png', 'jpg', 'jpeg', 'jfif', 'gif'] 
-					}, 
-					function(error: any, result: any) { 
-						if(error) console.log(error);
-						console.log(result); 
-					});
-
-
-				console.log(uploadImage);
-
-				const response = await prisma.user.update({
-					where: { mail: mail },
-					data: { 
-						name: user.name,
-						description: user.description,
-						mail: user.mail,
-						avatar: uploadImage.secure_url,
-						urlTikTok: user.urlTikTok,
-						urlInstagram: user.urlInstagram,
-						urlFaceBook: user.urlFaceBook,
-						keyWords: user.keyWords,
-					}
-				});
-				console.log(response);
-				if (response.active) {
-					return res.status(201).json(response);
-				} else {
-					return res
-						.status(404)
-						.json({ msg: 'Inactive user contact the admin' });
-				}
-			} catch (error: any) {
-				return res
-					.status(500)
-					.json({ error: error.message, name, description });
-			}
-		case 'DELETE':
-			try {
-				const response = await prisma.user.update({
-					where: { id: id?.toString() },
-					data: { active: false },
-				});
-				res.status(200).json({ User: response, msg: 'Suspendeed acount' });
-			} catch (error: any) {
-				return res
-					.status(500)
-					.json({ error: error.message, name, description });
-			}
-		default:
-			res.status(400).send('Method not supported try again');
-			break;
-	}
+      try {
+        const userInfo = await prisma.user.findUnique({
+          where: { mail: mail },
+        });
+        let uploadImage = null;
+        if (user.avatar) uploadImage = await cloudinaryImg(user);
+        const response = await prisma.user.update({
+          where: { mail: "alejandromanuelhernandez@gmail.com" },
+          data: {
+            name: user.name,
+            description: user.description,
+            mail: user.mail,
+            avatar: uploadImage ? uploadImage.secure_url : userInfo.avatar,
+            urlTikTok: user.urlTikTok,
+            urlInstagram: user.urlInstagram,
+            urlFaceBook: user.urlFaceBook,
+            keyWords: user.keyWords,
+          },
+        });
+        console.log(response);
+        if (response.active) {
+          return res.status(201).json(response);
+        } else {
+          return res
+            .status(404)
+            .json({ msg: "Inactive user contact the admin" });
+        }
+      } catch (error: any) {
+        return res
+          .status(500)
+          .json({ error: error.message, name, description });
+      }
+    case "DELETE":
+      try {
+        const response = await prisma.user.update({
+          where: { id: id?.toString() },
+          data: { active: false },
+        });
+        res.status(200).json({ User: response, msg: "Suspendeed acount" });
+      } catch (error: any) {
+        return res
+          .status(500)
+          .json({ error: error.message, name, description });
+      }
+    default:
+      res.status(400).send("Method not supported try again");
+      break;
+  }
 }

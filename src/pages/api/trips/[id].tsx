064ef292d9ddb790fe5e6f0f86e82adf9f0e3a29
MauroPerna <maurofabricioperna@gmail.com>
2022-09-16 17:00:31 -0300

    controllers trips in process

--- src/pages/api/trips/[id].tsx
+++ src/pages/api/trips/[id].tsx
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import TripsControllers from "src/controllers/trips";
 import { createUsers, createActivities, createCity } from "src/utils/interface";
 import prisma from "src/utils/prisma";
 
@@ -8,147 +9,92 @@ const id = async (
     req: NextApiRequest,
     res: NextApiResponse
 ) => {
-    const { method, body: { description, active, traveler, idPartaker, activitiesName, citiesIds }, query: { id } } = req;
-    switch (method) {
-        /**obtener info de un solo trp */
-        case 'GET':
-            let condition = {
-                where: {
-                    id: id.toString()
-                },
-                include: {
-                    planner: true,
-                    tripOnUser: {
-                        include: { user: true }
-                    },
-                    activitiesOnTrips: {
-                        include: { activity: true }
-                    },
-                    citiesOnTrips: {
-                        include: { city: true }
-                    }
+    const { method, body: { description, active, traveler, idPartaker, activitiesName, cities }, query: { id } } = req;
+    try {
+        switch (method) {
+            /**obtener info de un solo trp */
+            case 'GET':{
+                const response = await TripsControllers.getTrip({id});
+                return res.json(response);
+            }
+            /**actualizar untrip  */
+            case 'PUT':{
+                const response = await TripsControllers.putTrip({ description, active, traveler, idPartaker, activitiesName, cities }, { id })
+                return res.json(response);
+            }
+                
+            /**agrgar usuarios y actividades a un trip */
+            case 'PATCH':
+                if (!idPartaker && !Array.isArray(idPartaker) && !activitiesName && !Array.isArray(activitiesName) && !cities && !Array.isArray(cities)) {
+                    return res.status(400).json({ msg: 'Missing or invalid data, try again' })
                 }
-            }
-            try {
-                let response = await prisma.trip.findUnique(condition);
-                return res.status(200).json(response);
-            } catch (error: any) {
-                return res.status(404).json({ error: error.message });
-            }
-        /**actualizar untrip  */
-        case 'PUT':
-            try {
-                if (!active && !description && !traveler) {
-                    return res.status(400).json({ msg: 'Missing data try again' })
-                };
-                if (traveler) {
-                    await prisma.usersOnTrips.create({
+                let createUsers: createUsers[] = idPartaker ? idPartaker.map((idP: Object) => {
+                    return {
+                        user: {
+                            connect: {
+                                id: idP.toString()
+                            }
+                        }
+                    }
+                }) : [];
+                let createActivities: createActivities[] = activitiesName ? activitiesName.map((nameAct: string) => {
+                    return {
+                        activity: {
+                            connect: {
+                                name: nameAct
+                            }
+                        }
+                    }
+                }) : [];
+                let createCities: createCity[] = cities ? cities.map((idCity: string) => {
+                    return {
+                        city: {
+                            connect: {
+                                id: idCity.toString()
+                            }
+                        }
+                    }
+                }) : [];
+                try {
+                    return res.status(201).json(await prisma.trip.update({
+                        where: {
+                            id: id.toString()
+                        },
                         data: {
-                            userid: traveler,
-                            tripId: id.toLocaleString()
-                        }
-                    })
-                }
-                let trip = await prisma.trip.findUnique({ where: { id: id.toString() } });
-                if (!trip) return res.status(400).json({ msg: 'Trip not found, try again' });
-                let condition = {
-                    where: {
-                        id: id.toString()
-                    },
-                    data: {
-                        description: description ? description : trip.description,
-                        active: trip.active ? false : true,
-                    },
-                    include: {
-                        planner: true,
-                        tripOnUser: {
-                            include: { user: true, trip: true }
+                            tripOnUser: { create: createUsers },
+                            activitiesOnTrips: { create: createActivities },
+                            citiesOnTrips: { create: createCities }
                         },
-                        activitiesOnTrips: {
-                            include: { activity: true }
-                        },
-                        citiesOnTrips: {
-                            include: { city: true }
+                        include: {
+                            planner: true,
+                            tripOnUser: {
+                                include: { user: true, trip: true }
+                            },
+                            activitiesOnTrips: {
+                                include: { activity: true }
+                            },
+                            citiesOnTrips: {
+                                include: { city: true }
+                            }
                         }
-                    }
+                    }))
+                } catch (error: any) {
+                    return res.status(404).json({ error: error.message });
                 }
-                if (active !== undefined) {
-                    return res.status(201).json(await prisma.trip.update(condition));
-                } else {
-                    delete condition.data.active;
-                    return res.status(201).json(await prisma.trip.update(condition));
+            /**eliminar un trip  */
+            case 'DELETE':
+                try {
+                    return res.status(201).json(await prisma.trip.delete({ where: { id: id.toString() } }))
+                } catch (error: any) {
+                    return res.status(404).json({ error: error.message });
                 }
-            } catch (error: any) {
-                return res.status(404).json({ error: error.message });
-            }
-        /**agrgar usuarios y actividades a un trip */
-        case 'PATCH':
-            if (!idPartaker && !Array.isArray(idPartaker) && !activitiesName && !Array.isArray(activitiesName) && !citiesIds && !Array.isArray(citiesIds)) {
-                return res.status(400).json({ msg: 'Missing or invalid data, try again' })
-            }
-            let createUsers: createUsers[] = idPartaker ? idPartaker.map((idP: Object) => {
-                return {
-                    user: {
-                        connect: {
-                            id: idP.toString()
-                        }
-                    }
-                }
-            }) : [];
-            let createActivities: createActivities[] = activitiesName ? activitiesName.map((nameAct: string) => {
-                return {
-                    activity: {
-                        connect: {
-                            name: nameAct
-                        }
-                    }
-                }
-            }) : [];
-            let createCities: createCity[] = citiesIds ? citiesIds.map((idCity: string) => {
-                return {
-                    city: {
-                        connect: {
-                            id: idCity.toString()
-                        }
-                    }
-                }
-            }) : [];
-            try {
-                return res.status(201).json(await prisma.trip.update({
-                    where: {
-                        id: id.toString()
-                    },
-                    data: {
-                        tripOnUser: { create: createUsers },
-                        activitiesOnTrips: { create: createActivities },
-                        citiesOnTrips: { create: createCities }
-                    },
-                    include: {
-                        planner: true,
-                        tripOnUser: {
-                            include: { user: true, trip: true }
-                        },
-                        activitiesOnTrips: {
-                            include: { activity: true }
-                        },
-                        citiesOnTrips: {
-                            include: { city: true }
-                        }
-                    }
-                }))
-            } catch (error: any) {
-                return res.status(404).json({ error: error.message });
-            }
-        /**eliminar un trip  */
-        case 'DELETE':
-            try {
-                return res.status(201).json(await prisma.trip.delete({ where: { id: id.toString() } }))
-            } catch (error: any) {
-                return res.status(404).json({ error: error.message });
-            }
-        default:
-            return res.status(400).send('Method not supported try again');
+            default:
+                return res.status(400).send('Method not supported try again');
+        }
+    } catch (error) {
+        return res.status(500).json(error);
     }
+
 }
 
 export default id
--- src/pages/api/trips/index.tsx
+++ src/pages/api/trips/index.tsx
@@ -1,8 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import prisma from "src/utils/prisma";
-import cloudinary from 'src/utils/cloudinary';
-const { CLOUDINARY_PRESET_TRIPS } = process.env;
-import { condition, createActivities, createCity, createUsers, typeSort } from "src/utils/interface"
+import TripsControllers from 'src/controllers/trips'
 
 export default async function index(
     req: NextApiRequest,
@@ -23,181 +20,21 @@ export default async function index(
         },
         query: { wName, sort, sortBy, wActivity, wplanner, wCity, maxPrice }
     } = req;
-    switch (method) {
-        case 'GET':
-            let orderBy: typeSort[] = [];
-            let sortfrom: typeSort = {};
-            /**
-             * http://127.0.0.1:3000/api/trips?sort=asc&sortBy=price&wName=10&wplanner=cl7z6as0h01100cqiw589yste&maxPrice=7&wActivity=uno&wCity=Mex*/
-            let sortName: string = sortBy ? sortBy.toString().toLowerCase() : 'name';
-            sortfrom[sortName] = sort ? sort.toString().toLowerCase() : 'desc';
-            orderBy.push(sortfrom);
-            var condition: condition = {
-                where: wName ? { name: { contains: wName.toString() } } : {},
-                include: {
-                    planner: true,
-                    tripOnUser: {
-                        include: { user: true, trip: true }
-                    },
-                    activitiesOnTrips: {
-                        include: { activity: true }
-                    },
-                    citiesOnTrips: {
-                        include: { city: true }
-                    }
-                },
-                orderBy
-            };
-
-            wplanner ? condition.where = { ...condition.where, planner: { is: { id: wplanner.toString() } } } : '';
-            wActivity ?
-                condition.where = {
-                    ...condition.where,
-                    activitiesOnTrips: {
-                        some: {
-                            activity: {
-                                is: {
-                                    name: {
-                                        contains: wActivity.toString()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } : '';
-            wCity ?
-                condition.where = {
-                    citiesOnTrips: {
-                        some: {
-                            city: {
-                                is: {
-                                    name: {
-                                        contains: wCity.toString()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } : '';
-            maxPrice ? condition.where = { ...condition.where, price: { lte: parseFloat(maxPrice.toString()) } } : '';
-
-            try {
-                const response = await prisma.trip.findMany(condition);
-                return res.status(200).json(response)
-            } catch (error: any) {
-                return res.status(500).json({ error: error.message });
+    try { 
+        switch (method) {
+            case 'GET':{
+                const response = await TripsControllers.getTrips({ wName, sort, sortBy, wActivity, wplanner, wCity, maxPrice })
+                return res.json(response);
             }
-        case 'POST':
-            if (
-                !name ||
-                !initDate ||
-                !endDate ||
-                !description ||
-                !price ||
-                !planner ||
-                !activitiesName ||
-                !Array.isArray(activitiesName) ||
-                !cities ||
-                !Array.isArray(cities)
-            ) {
-                return res.status(400).json({ msg: 'Missing or invalid data, try again' })
+            case 'POST':{
+                const response = await TripsControllers.postTrip({name, initDate, endDate, planner, description, price, activitiesName, image, cities})
+                return res.json(response)
             }
-            let initialDate = new Date(initDate);
-            let finishDate = new Date(endDate);
-
-            // idPartaker,
-            // (idPartaker && !Array.isArray(idPartaker)) ||
-            // let createUsers: createUsers[] = idPartaker ? idPartaker.map((idP: string) => {
-            //     return {
-            //         user: {
-            //             connect: {
-            //                 id: idP.toString()
-            //             }
-            //         }
-            //     }
-            // }) : [];
-            // tripOnUser: { create: createUsers },
-            // tripOnUser: {
-            //     include: { user: true, trip: true }
-            // },
-
-
-            let createActivities: createActivities[] = activitiesName ? activitiesName.map((nameAct: string) => {
-                return {
-                    activity: {
-                        connect: {
-                            name: nameAct
-                        }
-                    }
-                }
-            }) : [];
-            let createCities: createCity[] = cities ? cities.map((nameCity: string) => {
-                return {
-                    city: {
-                        connect: {
-                            name: nameCity.toString()
-                        }
-                    }
-                }
-            }) : [];
-            try {
-
-                 const uploadImage = await cloudinary.uploader.upload(image,
-                     {
-                         upload_preset: CLOUDINARY_PRESET_TRIPS,
-                         public_id: `${name}-image:${Date.now()}`,
-                         allowed_formats: ['png', 'jpg', 'jpeg', 'jfif', 'gif']
-                     },
-                     function (error: any, result: any) {
-                         if (error) console.log(error);
-                         console.log(result);
-                     });
-
-                const response = await prisma.trip.create({
-                    data: {
-                        name: name.toLowerCase(),
-                        initDate: initialDate,
-                        endDate: finishDate,
-                        description: description,
-                        price: price,
-                        plannerId: planner,
-                        image: uploadImage.secure_url,
-                        public_id_image: uploadImage.public_id,
-                        activitiesOnTrips: { create: createActivities },
-                        citiesOnTrips: { create: createCities }
-                    },
-                    include: {
-                        planner: true,
-                        activitiesOnTrips: {
-                            include: { activity: true }
-                        },
-                        citiesOnTrips: {
-                            include: { city: true }
-                        }
-                    }
-                });
-                return res.status(201).json(response);
-            } catch (error: any) {
-                console.log(error);
-                return res.status(500).json({ error, name, description });
-            }
-        /** Forma en la que se envia la informasión
-         * idPartaker se queda comentado 
-         * activitiesName y citiesName es necesario contar con almenos uno
-             {
-                "name": "Trip de pruebas",
-                "initDate": "2022-06-15",
-                "endDate": "2022-12-12",
-                "planner": "cl82bc4ow0091auqirz3xjdxv",
-                "description": "Pruebas...",
-                "price": 15.30,
-                "image": "Pruebas de imagen",
-                "activitiesName": ["Chichen Itza"],
-                "citiesName": ["Toluca"]
-            }
-        */
-        default:
-            res.status(400).send('Method not supported try again')
-            break;
+            default:
+                res.status(400).send('Method not supported try again')
+                break;
+        }
+    } catch (error) {
+        res.status(500).json(error)
     }
 }

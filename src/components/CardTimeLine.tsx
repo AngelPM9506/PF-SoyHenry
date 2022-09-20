b71fc4802db14545b11c0919b16bdb47ededbdc3
dinucciagus <dinucciagus@gmail.com>
2022-09-20 06:53:05 -0300

    detail trip with timeline

---
+++ src/components/CardTimeLine.tsx
@@ -0,0 +1,64 @@
+import { Text, HStack, VStack, Button, Image, Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useQuery } from "react-query";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import { MdOutlineTripOrigin } from "react-icons/md";
+
+const url =
+  "https://res.cloudinary.com/mauro4202214/image/upload/v1663527844/world-travelers/activitydefault_q9aljz.png";
+
+export default function CardTimeLine({ activity, cities }: any) {
+  return (
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      contentStyle={{
+        background: "#D1DFE3",
+        color: "white",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid #D1DFE3" }}
+      date="20/07/2022"
+      iconStyle={{ background: "#293541", color: "#02b1b1" }}
+      icon={<MdOutlineTripOrigin />}
+    >
+      <HStack height={"150px"} width={"100%"} justifyContent={"space-around"}>
+        <Image
+          src={activity?.image != null ? activity?.image : url}
+          alt={activity?.name}
+          rounded={"lg"}
+          boxShadow={"0px 10px 13px -7px #000000"}
+          height={"100%"}
+        />
+        <VStack align={"center"}>
+          <Heading
+            font-size={"20px"}
+            paddingBottom={"15px"}
+            color={"#293541"}
+            fontFamily={"body"}
+            fontWeight={500}
+          >
+            {activity?.name}
+          </Heading>
+
+          {cities?.map((c) => {
+            if (c.city.id === activity?.cityId) {
+              return (
+                <Text
+                  font-size={"15px"}
+                  paddingBottom={"15px"}
+                  color={"#293541"}
+                >
+                  {c.city.name}
+                </Text>
+              );
+            }
+          })}
+          <NextLink href={`/activities/${activity.id}`}>
+            <Button bg={"#02b1b1"} color={"#293541"}>
+              see more info of this activity
+            </Button>
+          </NextLink>
+        </VStack>
+      </HStack>
+    </VerticalTimelineElement>
+  );
+}
---
+++ src/components/TimeLineTrip.tsx
@@ -0,0 +1,83 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { MdOutlineTripOrigin } from "react-icons/md";
+import {
+  HStack,
+  Image,
+  Link,
+  Text,
+  VStack,
+  Button,
+  Stack,
+  Box,
+  StackDivider,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import CardTimeLine from "./CardTimeLine";
+
+export const TimeLine = ({ data }: any) => {
+  return (
+    <Stack width={"100%"} align={"center"}>
+      <StackDivider borderColor={useColorModeValue("gray.200", "gray.600")} />
+      <Box
+        bgColor={"#02b1b1"}
+        height={"60px"}
+        width={"230px"}
+        rounded={"lg"}
+        padding={"8px"}
+        boxShadow={"0px 5px   white"}
+      >
+        <Text textAlign={"center"} fontWeight={"bold"} fontSize={"2xl"}>
+          Trip's itinerary
+        </Text>
+      </Box>
+      <VerticalTimeline>
+        {data.activitiesOnTrips.length != 0 ? (
+          data.activitiesOnTrips.map((activity: any) => (
+            <CardTimeLine
+              activity={activity.activity}
+              cities={data.citiesOnTrips}
+            />
+          ))
+        ) : (
+          <Text> No Activities found</Text>
+        )}
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          iconStyle={{ background: "#293541", color: "#02b1b1" }}
+          icon={<MdOutlineTripOrigin />}
+        />
+      </VerticalTimeline>
+      <Stack alignItems={"center"} justifyContent={"center"}>
+        <NextLink href={"/pasarella"}>
+          <Button
+            rounded={"lg"}
+            w={"100%"}
+            pb={"2px"}
+            mb={"10px"}
+            mt={"0px"}
+            size={"lg"}
+            py={"8"}
+            bg={useColorModeValue("gray.900", "gray.50")}
+            color={useColorModeValue("white", "gray.900")}
+            textTransform={"uppercase"}
+            fontSize={"xl"}
+            _hover={{
+              transform: "translateY(-2px)",
+              boxShadow: "lg",
+              bg: "#F3B46F",
+              color: "#293541",
+            }}
+          >
+            Pay and Join the trip!
+          </Button>
+        </NextLink>
+      </Stack>
+    </Stack>
+  );
+};

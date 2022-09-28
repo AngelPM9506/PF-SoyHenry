e648b3605d71ffadeb8b55072b06eaacd835d916
AngelPM9506 <parra_mondragon@live.com>
2022-09-28 13:43:08 -0500

    Chat simple listo

---
+++ src/pages/api/socket/index.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { Server } from 'socket.io';
+const index = (req: NextApiRequest, res: NextApiResponse | any) => {
+    if (res.socket.server.io) {
+        console.log('server is already running');
+    } else {
+        console.log('Socket is initializing');
+        const io = new Server(res.socket.server);
+        res.socket.server.io = io;
+        io.on('connection', socket => {
+            socket.on('input-change', msg => {
+                console.log(msg);
+                
+                socket.broadcast.emit('update-input', msg);
+            });
+        })
+    }
+    res.status(200).end();
+}
+
+export default index

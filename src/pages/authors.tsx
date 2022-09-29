57c903be261463abb41fb8e06b0ab1b2d7c86db7
dinucciagus <dinucciagus@gmail.com>
2022-09-29 18:28:06 -0300

    design authors

--- src/pages/authors.tsx
+++ src/pages/authors.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
 import Layout from "src/components/layout/Layout";
 import AuthorCard from "../components/authorCard";
@@ -7,20 +7,118 @@ const authors = () => {
   return (
     <Layout>
       <NextSeo title="World Travelers" />
-      <Box> Autores</Box>
-      <AuthorCard
-        name={"Agustina Di Nucci"}
-        job={"Full Stack Web Developer"}
-        linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
-        github={"https://github.com/dinucciagus"}
-        cv={"https://github.com/dinucciagus"}
-        avatar={
-          "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
-        }
-        image={
-          "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
-        }
-      />
+      <Stack
+        alignSelf={"center"}
+        width={"100vw"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Stack
+          width={"60%"}
+          bg={"RGBA(209,223,227,0.1)"}
+          display={"flex"}
+          justifyContent={"space-around"}
+          rounded={"20px"}
+          marginTop={"40px"}
+        >
+          <Text>Meet the developers team:</Text>
+          <SimpleGrid
+            columns={3}
+            columnGap={"6"}
+            rowGap={"6"}
+            width={"100%"}
+            paddingLeft={"20px"}
+            paddingRight={"20px"}
+            alignSelf={"center"}
+          >
+            <AuthorCard
+              name={"Agustina Di Nucci"}
+              job={"Full Stack Web Developer"}
+              linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
+              github={"https://github.com/dinucciagus"}
+              cv={"https://github.com/dinucciagus"}
+              avatar={
+                "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
+              }
+              image={
+                // "http://drive.google.com/uc?export=view&id=1_qsAn-4e7xBfc_IGLbtQMt9Sgo29WjUv"
+                "http://drive.google.com/uc?export=view&id=1CczbOs_-ph6f4sbWzxZwMJcPDWpahD2l"
+              }
+            />
+            <AuthorCard
+              name={"Agustina Di Nucci"}
+              job={"Full Stack Web Developer"}
+              linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
+              github={"https://github.com/dinucciagus"}
+              cv={"https://github.com/dinucciagus"}
+              avatar={
+                "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
+              }
+              image={
+                // "http://drive.google.com/uc?export=view&id=1_qsAn-4e7xBfc_IGLbtQMt9Sgo29WjUv"
+                "http://drive.google.com/uc?export=view&id=1CczbOs_-ph6f4sbWzxZwMJcPDWpahD2l"
+              }
+            />
+            <AuthorCard
+              name={"Agustina Di Nucci"}
+              job={"Full Stack Web Developer"}
+              linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
+              github={"https://github.com/dinucciagus"}
+              cv={"https://github.com/dinucciagus"}
+              avatar={
+                "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
+              }
+              image={
+                // "http://drive.google.com/uc?export=view&id=1_qsAn-4e7xBfc_IGLbtQMt9Sgo29WjUv"
+                "http://drive.google.com/uc?export=view&id=1CczbOs_-ph6f4sbWzxZwMJcPDWpahD2l"
+              }
+            />
+            <AuthorCard
+              name={"Agustina Di Nucci"}
+              job={"Full Stack Web Developer"}
+              linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
+              github={"https://github.com/dinucciagus"}
+              cv={"https://github.com/dinucciagus"}
+              avatar={
+                "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
+              }
+              image={
+                // "http://drive.google.com/uc?export=view&id=1_qsAn-4e7xBfc_IGLbtQMt9Sgo29WjUv"
+                "http://drive.google.com/uc?export=view&id=1CczbOs_-ph6f4sbWzxZwMJcPDWpahD2l"
+              }
+            />
+            <AuthorCard
+              name={"Agustina Di Nucci"}
+              job={"Full Stack Web Developer"}
+              linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
+              github={"https://github.com/dinucciagus"}
+              cv={"https://github.com/dinucciagus"}
+              avatar={
+                "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
+              }
+              image={
+                // "http://drive.google.com/uc?export=view&id=1_qsAn-4e7xBfc_IGLbtQMt9Sgo29WjUv"
+                "http://drive.google.com/uc?export=view&id=1CczbOs_-ph6f4sbWzxZwMJcPDWpahD2l"
+              }
+            />
+            <AuthorCard
+              name={"Agustina Di Nucci"}
+              job={"Full Stack Web Developer"}
+              linkedin={"https://www.linkedin.com/in/agustina-dinucci/"}
+              github={"https://github.com/dinucciagus"}
+              cv={"https://github.com/dinucciagus"}
+              avatar={
+                "https://avatars.githubusercontent.com/u/105603671?s=400&u=9dae9f01a131d632cb953afee34917054febfca1&v=4"
+              }
+              image={
+                // "http://drive.google.com/uc?export=view&id=1_qsAn-4e7xBfc_IGLbtQMt9Sgo29WjUv"
+                "http://drive.google.com/uc?export=view&id=1CczbOs_-ph6f4sbWzxZwMJcPDWpahD2l"
+              }
+            />
+          </SimpleGrid>
+        </Stack>
+      </Stack>
     </Layout>
   );
 };

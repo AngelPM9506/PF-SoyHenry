625570658572053a06f0f113530ae7af279ba297
AngelPM9506 <parra_mondragon@live.com>
2022-09-29 08:39:30 -0500

    modificacion en cocketio back

--- src/pages/api/socket/index.ts
+++ src/pages/api/socket/index.ts
@@ -8,8 +8,18 @@ const index = (req: NextApiRequest, res: NextApiResponse | any) => {
         const io = new Server(res.socket.server);
         res.socket.server.io = io;
         io.on('connection', socket => {
-            socket.on('input-change', msg => {
-                socket.broadcast.emit('update-input', msg);
+            /**desconeccion del chat */
+            socket.on('disconnect', () => console.log(`Disconect: ${socket.id}`));
+
+            /** Union a un chat */
+            socket.on('join', room => {
+                socket.join(room);
+            });
+
+            /**Enviar mensaje a un room */
+            socket.on('chat', data => {
+                const { message, room } = data;
+                io.to(room).emit('chat', message);
             });
         })
     }
@@ -17,3 +27,20 @@ const index = (req: NextApiRequest, res: NextApiResponse | any) => {
 }
 
 export default index
+/** ejemplo de la pagina 
+ * https://tincode.es/blog/manejo-de-salas-con-socket-io-y-react-hooks
+io.on('connection', (socket) => {
+    console.log(`Connected: ${socket.id}`);
+    socket.on('disconnect', () =>
+        console.log(`Disconnected: ${socket.id}`));
+    socket.on('join', (room) => {
+        console.log(`Socket ${socket.id} joining ${room}`);
+        socket.join(room);
+    });
+    socket.on('chat', (data) => {
+        const { message, room } = data;
+        console.log(`msg: ${message}, room: ${room}`);
+        io.to(room).emit('chat', message);
+    });
+});
+*/

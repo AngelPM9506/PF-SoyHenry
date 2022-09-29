4f991a11b1d992226c804931483ba65bf8e7be6f
JonathanJB88 <Jonajes0288@gmail.com>
2022-09-29 18:35:28 +0200

    Redesign of user details

--- src/components/UserDetail.tsx
+++ src/components/UserDetail.tsx
@@ -8,16 +8,12 @@ import {
   Stack,
   Text,
   useColorModeValue,
-  Avatar,
   Box,
-  Wrap,
-  WrapItem,
-  Grid,
-  GridItem,
 } from "@chakra-ui/react";
 import { Trip } from "src/utils/interface";
 import { UserData } from "./UserProfile";
-import NextLink from "next/link";
+import AvatarCarousel from "./AvatarCarousel";
+import AvaCarousel from "./AvaCarousel";
 
 interface Props {
   userDetail: UserData;
@@ -36,10 +32,6 @@ export const UserDetail = ({ userDetail, trips }: Props) => {
   const facebook: string =
     "https://i0.wp.com/www.dpabogados.com/wp-content/uploads/2016/09/facebook-logo-png-transparent-background.png?fit=1600%2C1600&ssl=1";
 
-  const tikTokPage = "https://www.tiktok.com/";
-  const instaPage = "https://www.instagram.com/";
-  const facePage = "https://es-es.facebook.com/";
-
   const defaulHashtags: string[] = ["trips", "traveling", "friends"];
   const arrInterests: string[] = user.keyWords
     ? user.keyWords.split(",")
@@ -55,40 +47,36 @@ export const UserDetail = ({ userDetail, trips }: Props) => {
     .map((trip) => trip.trip);
 
   return (
-    <>
+    <Stack display={"flex"} justifyContent={"center"} alignItems={"center"}>
       <Center pt={6}>
         <Heading>Meet the traveler</Heading>
       </Center>
-
-      <Grid
-        templateAreas={`
-                  "nav main"
-                  "nav footer"`}
-        gridTemplateRows={"14vw 1fr 1vh"}
-        gridTemplateColumns={"45vw 1fr"}
-        h="60vh"
-        gap="1"
-        margin={"2%"}
-        fontWeight="bold"
-      >
-        <GridItem pl="2" area={"nav"}>
-          <Center py={2} h={"55vh"}>
+      <Center>
+        <Stack
+          direction={{ base: "column", md: "column", lg: "row" }}
+          spacing={4}
+          p={6}
+          width={"100vw"}
+          height={{ base: "auto", md: "auto", lg: "auto" }}
+          justifyContent={"center"}
+        >
+          <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
             <Stack
               borderWidth="1px"
               borderRadius="lg"
               w={{ sm: "100%", md: "540px" }}
-              height={{ sm: "476px", md: "20rem" }}
+              height={{ sm: "100%", md: "100%" }}
               direction={{ base: "column", md: "row" }}
               bg={useColorModeValue("white", "gray.900")}
               boxShadow={"2xl"}
               padding={4}
             >
-              <Flex flex={1}>
+              <Flex justifyContent={"center"} flex={1}>
                 <Image
                   alt="image user"
                   borderRadius={"xl"}
                   objectFit="cover"
-                  boxSize="100%"
+                  boxSize={{ sm: "50%", md: "100%", lg: "100%" }}
                   src={user?.avatar ? user.avatar.toString() : logo}
                 />
               </Flex>
@@ -130,6 +118,7 @@ export const UserDetail = ({ userDetail, trips }: Props) => {
                     );
                   })}
                 </Stack>
+
                 <Center>
                   <Stack
                     width={"100%"}
@@ -139,132 +128,78 @@ export const UserDetail = ({ userDetail, trips }: Props) => {
                     justifyContent={"space-between"}
                     alignItems={"center"}
                   >
-                    <a
-                      href={user?.urlTikTok ? user.urlTikTok : tikTokPage}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Image
-                        boxSize="40px"
-                        objectFit="cover"
-                        src={tikTok}
-                        alt="tikTok-icon"
-                      />
-                    </a>
-                    <a
-                      href={user?.urlInstagram ? user.urlInstagram : instaPage}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Image
-                        boxSize="40px"
-                        objectFit="cover"
-                        src={instagram}
-                        alt="instagram-icon"
-                      />
-                    </a>
-                    <a
-                      href={user?.urlFaceBook ? user.urlFaceBook : facePage}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Image
-                        boxSize="40px"
-                        objectFit="cover"
-                        src={facebook}
-                        alt="facebook-icon"
-                      />
-                    </a>
+                    {user?.urlTikTok && (
+                      <a href={user.urlTikTok} target="_blank" rel="noreferrer">
+                        <Image
+                          boxSize="40px"
+                          objectFit="cover"
+                          src={tikTok}
+                          alt="tikTok-icon"
+                        />
+                      </a>
+                    )}
+                    {user?.urlInstagram && (
+                      <a
+                        href={user?.urlInstagram}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Image
+                          boxSize="40px"
+                          objectFit="cover"
+                          src={instagram}
+                          alt="instagram-icon"
+                        />
+                      </a>
+                    )}
+                    {user?.urlFaceBook && (
+                      <a
+                        href={user?.urlFaceBook}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Image
+                          boxSize="40px"
+                          objectFit="cover"
+                          src={facebook}
+                          alt="facebook-icon"
+                        />
+                      </a>
+                    )}
                   </Stack>
                 </Center>
               </Stack>
             </Stack>
-          </Center>
-        </GridItem>
-        <GridItem pl="2" area={"main"}>
-          <Text
-            textAlign={"center"}
-            color={useColorModeValue("#293541", "#F3B46F")}
-            fontSize={"xl"}
-            fontFamily={"body"}
-            p={2}
-          >
-            Trips created by this traveler
-          </Text>
-          <Wrap align={"center"} justify={"center"}>
-            {myCreatedActiveTrips?.map((trip) => {
-              return (
-                <WrapItem key={trip.id}>
-                  <Box
-                    margin={2}
-                    display={"flex"}
-                    key={trip.id}
-                    flexDirection={"column"}
-                  >
-                    <NextLink href={`/trips/${trip.id}`}>
-                      <Avatar
-                        cursor={"pointer"}
-                        alignSelf={"center"}
-                        name={trip.name}
-                        rounded={"full"}
-                        size={"lg"}
-                        objectFit={"cover"}
-                        src={trip.image ? trip.image.toString() : logo}
-                      />
-                    </NextLink>
-                    <Text noOfLines={2} textAlign={"center"} fontSize={"sm"}>
-                      {" "}
-                      {trip.name}{" "}
-                    </Text>
-                  </Box>
-                </WrapItem>
-              );
-            })}
-          </Wrap>
-        </GridItem>
-        <GridItem pl="2" area={"footer"}>
-          <Text
-            textAlign={"center"}
-            color={useColorModeValue("#293541", "#F3B46F")}
-            fontSize={"xl"}
-            fontFamily={"body"}
-            p={2}
-          >
-            Trips traveler joined
-          </Text>
-          <Wrap align={"center"} justify={"center"}>
-            {tripsTravJoined?.map((t: Trip) => {
-              return (
-                <WrapItem key={t.id}>
-                  <Box
-                    margin={2}
-                    display={"flex"}
-                    key={t.id}
-                    flexDirection={"column"}
-                  >
-                    <NextLink href={`/trips/${t.id}`}>
-                      <Avatar
-                        cursor={"pointer"}
-                        alignSelf={"center"}
-                        name={t.name}
-                        rounded={"full"}
-                        size={"lg"}
-                        objectFit={"cover"}
-                        src={t.image ? t.image.toString() : logo}
-                      />
-                    </NextLink>
-                    <Text noOfLines={2} textAlign={"center"} fontSize={"sm"}>
-                      {" "}
-                      {t.name}{" "}
-                    </Text>
-                  </Box>
-                </WrapItem>
-              );
-            })}
-          </Wrap>
-        </GridItem>
-      </Grid>
-    </>
+          </Box>
+          <Box display={"flex"} justifyContent={"center"} flexDir={"column"}>
+            <Text
+              textAlign={"center"}
+              color={useColorModeValue("#293541", "#F3B46F")}
+              fontSize={"xl"}
+              fontFamily={"body"}
+              p={2}
+            >
+              Trips created by this traveler
+            </Text>
+            <Box display={"flex"} justifyContent={"center"}>
+              <AvatarCarousel trips={myCreatedActiveTrips} />
+            </Box>
+            <Text
+              textAlign={"center"}
+              color={useColorModeValue("#293541", "#F3B46F")}
+              fontSize={"xl"}
+              fontFamily={"body"}
+              p={2}
+            >
+              Trips traveler joined
+            </Text>
+            <Box display={"flex"} justifyContent={"center"}>
+              <AvaCarousel trips={tripsTravJoined} />
+            </Box>
+          </Box>
+        </Stack>
+      </Center>
+    </Stack>
   );
 };
 

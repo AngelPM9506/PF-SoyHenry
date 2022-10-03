21153c83e2cfdcacc375af7363a212ce3ac47997
dinucciagus <dinucciagus@gmail.com>
2022-10-03 15:12:54 -0300

    banned

--- src/components/Banned.tsx
+++ src/components/Banned.tsx
@@ -1,57 +1,90 @@
-import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
-  Box,
-  Button,
-  CloseButton,
-  Image,
-  Link,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Box, Button, Image, Link, Text } from "@chakra-ui/react";
+import Layout from "src/components/layout/Layout";
+import NextLink from "next/link";
+import { NextSeo } from "next-seo";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
-export const BannedAlert = () => {
-  const {
-    isOpen: isVisible,
-    onClose,
-    onOpen,
-  } = useDisclosure({ defaultIsOpen: true });
+const Banned = () => {
   return (
-    <Box>
-      <Alert status="error" variant="solid">
-        <AlertIcon />
-        <Box>
-          <AlertTitle fontSize={"large"}>You are suspended</AlertTitle>
-          <AlertDescription fontSize={"large"}>
-            Your account has been suspended, please reach us at{" "}
-            <b>contact@wordtravelers.com</b> for more information.
-          </AlertDescription>
-        </Box>
-      </Alert>
+    <Layout>
+      <NextSeo title="404" />
       <Box
-        width={"100%"}
+        alignSelf={"center"}
+        width={"90%"}
+        height={"100%"}
+        minHeight={"75vh"}
         display={"flex"}
+        flexDirection={"column"}
         justifyContent={"center"}
         alignContent={"center"}
       >
         <Box
-          margin={"30px"}
+          marginTop={"10px"}
+          marginBottom={"10px"}
+          display={"flex"}
+          flexDirection={"column"}
+          alignSelf={"center"}
           justifyContent={"center"}
           alignItems={"center"}
-          width={"65%"}
+          width={{ base: "100%", md: "75%" }}
+          height={{ base: "max-content", lg: "76vh" }}
           position="relative"
           rounded="2xl"
           boxShadow="2xl"
           overflow="hidden"
+          background={"#D1DFE3"}
+          padding={"10%"}
         >
           <Image
             objectFit={"cover"}
-            src="https://i.vgy.me/zG6dQP.png"
+            rounded={"100%"}
+            height={"200px"}
+            src={
+              "http://drive.google.com/uc?export=view&id=1rrVsOOcFpvgBLmUc29AJOJzyY-eZRDRZ"
+            }
             alt={"Error 404 not found"}
           />
+          <Text
+            textAlign={"center"}
+            width="100%"
+            color="#293541"
+            marginTop={"5px"}
+            fontSize={{ base: "2xl", lg: "3xl" }}
+            fontWeight={"bold"}
+          >
+            Your account has been temporarily suspended from World Travelers
+            because you may have violated our terms of service.
+          </Text>
+          <Text
+            textAlign={"center"}
+            width="100%"
+            color="#293541"
+            fontSize={{ base: "xl", lg: "2xl" }}
+            justifyContent={"center"}
+          >
+            If you think this can be a mistake, please write to us here!
+          </Text>
+          <Box height={"50px"}>
+            <AiOutlineArrowDown size="100%" color="#293541" />
+          </Box>
+          <Button
+            alignSelf={"center"}
+            bg={"#02b1b1"}
+            color={"white"}
+            fontSize={{ base: "md", md: "xl" }}
+            rounded={"md"}
+            padding={{ base: "8px", md: "20px", lg: "30px" }}
+            _hover={{
+              transform: "translateY(-2px)",
+              boxShadow: "lg",
+            }}
+          >
+            <NextLink href={"/contact"}> Leave a message</NextLink>
+          </Button>
         </Box>
       </Box>
-    </Box>
+    </Layout>
   );
 };
+
+export default Banned;
